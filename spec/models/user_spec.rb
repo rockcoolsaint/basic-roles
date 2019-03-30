03aeb2f91a8f9d6476030b80b4e8bdf0b3dcea59
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2019-03-30 10:42:36 +0100

    super user and basic user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,11 @@ RSpec.describe User, type: :model do
   end
 
   describe '#has_role?' do
-    let(:user) { create(:user) }
+    let(:super_user) { create(:user) }
+    let(:basic_user) { create(:user, :basic) }
 
-    it { expect(user.has_role?('super')).to eq true }
+    it { expect(super_user.has_role?('super')).to eq true }
+    it { expect(basic_user.has_role?('basic')).to eq true }
   end
 
   describe 'super user email' do
